62d2829eab013e4175a4525afed6ffc91ccc1a3a
Xt-Andrey <andreygalvis6002@gmail.com>
2026-05-30 11:40:52 -0500

    Refactor main.js for cleaner import and app creation

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
+/* eslint-disable */
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/Index.js'
 
-import router from './router/Index'
-import './assets/css/Styles.css'     
+import './assets/css/Styles.css'
 import './assets/css/Styles/Header.css'
 import './assets/css/Styles/Footer.css'
 import './assets/css/Styles/Sidebar.css'
 import './assets/css/Login.css'
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+createApp(App).use(router).mount('#app')
